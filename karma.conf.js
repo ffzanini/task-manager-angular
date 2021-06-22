fec4a30daf6c074aca77d4b642f54915f66bafad
Felipe Frantz Zanini <felipe.zanini@nave.rs>
2021-06-21 21:45:06 -0300

    rewrite readme

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-crash'),
+      dir: require('path').join(__dirname, './coverage/task-manager-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-crash'`, () => {
+  it(`should have as title 'task-manager-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-crash');
+    expect(app.title).toEqual('task-manager-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-crash app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('task-manager-angular app is running!');
   });
 });
