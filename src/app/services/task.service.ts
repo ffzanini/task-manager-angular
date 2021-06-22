fa8606f61b07a31d5c7313d274f37812fddbd330
Felipe Frantz Zanini <felipe.zanini@nave.rs>
2021-06-21 21:36:48 -0300

    first commit

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../Task';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiURL = 'http://localhost:5000/tasks'
+  constructor(private http:HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiURL)
+  }
+
+  deleteTask(task: Task) : Observable<Task> {
+    const url = `${this.apiURL}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task) : Observable<Task> {
+    const url = `${this.apiURL}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTask(task: Task) : Observable<Task> {
+    return this.http.post<Task>(this.apiURL, task, httpOptions);
+  }
+}
